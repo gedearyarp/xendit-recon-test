4def1bb85b479fa03162268939676990e1aaa606
I Gede Arya Raditya Parameswara <gdryrp@gmail.com>
2022-07-20 18:45:13 +0700

    add main logic

--- main.go
+++ main.go
@@ -8,23 +8,19 @@ import (
 
 func main() {
 	var (
-		sourceCsv    svc.CsvService
-		proxyCsv     svc.CsvService
-		reconService svc.ReconService
-
 		sources      []m.Source
 		proxies      []m.Proxy
 		reconResults []m.ReconResult
 	)
 
-	sourceCsv = svc.CsvService{FileName: "file/source.csv"}
-	proxyCsv = svc.CsvService{FileName: "file/proxy.csv"}
+	sourceCsv := svc.CsvService{FileName: "file/source.csv"}
+	proxyCsv := svc.CsvService{FileName: "file/proxy.csv"}
 
 	sources = sourceCsv.ReadSource()
 	proxies = proxyCsv.ReadProxy()
 
-	reconService = svc.ReconService{Sources: sources, Proxies: proxies}
-	reconResults = reconService.Compare()
+	reconService:= svc.ReconService{Sources: sources, Proxies: proxies}
+	reconResults = reconService.Perform()
 
-	fmt.Println(reconService)
+	fmt.Println(reconResults)
 }
--- service/recon_service.go
+++ service/recon_service.go
@@ -9,8 +9,125 @@ type ReconService struct {
 	Proxies []m.Proxy
 }
 
-func (svc *ReconService) Compare() []m.ReconResult {
+type ReconData struct {
+	SourceMap map[string]m.Source
+	ProxyMap  map[string]m.Proxy
+}
+
+func (svc *ReconService) Perform() []m.ReconResult {
+	reconData := ReconData{
+		SourceMap: MapSources(svc.Sources),
+		ProxyMap:  MapProxies(svc.Proxies),
+	}
+
+	return reconData.Compare()
+}
+
+func MapSources(sources []m.Source) map[string]m.Source {
+	var sourceMap = make(map[string]m.Source)
+
+	for _, source := range sources {
+		sourceMap[source.ID] = source
+	}
+
+	return sourceMap
+}
+
+func MapProxies(proxies []m.Proxy) map[string]m.Proxy {
+	var proxyMap = make(map[string]m.Proxy)
+
+	for _, proxy := range proxies {
+		proxyMap[proxy.ID] = proxy
+	}
+
+	return proxyMap
+}
+
+func (recon *ReconData) Compare() []m.ReconResult {
 	var result []m.ReconResult
 
+	proxyToSourceRecon := recon.ProxyToSource()
+	result = append(result, proxyToSourceRecon...)
+
+	sourceToProxyRecon := recon.SourceToProxy()
+	result = append(result, sourceToProxyRecon...)
+
+	return result
+}
+
+func (recon *ReconData) ProxyToSource() []m.ReconResult {
+	var (
+		result    []m.ReconResult
+		reconData m.ReconResult
+	)
+
+	for key, value := range recon.ProxyMap {
+		source, ok := recon.SourceMap[key]
+		if !ok {
+			reconData = m.ReconResult{
+				ID:          value.ID,
+				Amount:      value.Amount,
+				Description: value.Description,
+				Date:        value.Date,
+				Remark:      m.SOURCE_NOT_FOUND,
+			}
+			result = append(result, reconData)
+		} else {
+			if value.Amount != source.Amount {
+				reconData = m.ReconResult{
+					ID:          value.ID,
+					Amount:      value.Amount,
+					Description: value.Description,
+					Date:        value.Date,
+					Remark:      m.AMOUNT_DIFF,
+				}
+				result = append(result, reconData)
+			}
+
+			if value.Date != source.Date {
+				reconData = m.ReconResult{
+					ID:          value.ID,
+					Amount:      value.Amount,
+					Description: value.Description,
+					Date:        value.Date,
+					Remark:      m.DATE_DIFF,
+				}
+				result = append(result, reconData)
+			}
+
+			if value.Description != source.Description {
+				reconData = m.ReconResult{
+					ID:          value.ID,
+					Amount:      value.Amount,
+					Description: value.Description,
+					Date:        value.Date,
+					Remark:      m.DATE_DIFF,
+				}
+				result = append(result, reconData)
+			}
+		}
+	}
+	return result
+}
+
+func (recon *ReconData) SourceToProxy() []m.ReconResult {
+	var (
+		result    []m.ReconResult
+		reconData m.ReconResult
+	)
+
+	for key, value := range recon.SourceMap {
+		_, ok := recon.ProxyMap[key]
+		if !ok {
+			reconData = m.ReconResult{
+				ID:          value.ID,
+				Amount:      value.Amount,
+				Description: value.Description,
+				Date:        value.Date,
+				Remark:      m.PROXY_NOT_FOUND,
+			}
+			result = append(result, reconData)
+		}
+	}
 	return result
 }
